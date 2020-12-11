1c742d35f3fd5033f2819270ff96f5eacc5d6d7f
David V. Lu <davidvlu@gmail.com>
2020-12-11 14:54:57 -0500

    Rebase from 'upstream'

--- locomotor/src/locomotor.cpp
+++ locomotor/src/locomotor.cpp
@@ -35,6 +35,7 @@
 #include <locomotor/locomotor.h>
 #include <nav_2d_utils/tf_help.h>
 #include <functional>
+#include <memory>
 #include <string>
 
 namespace locomotor
@@ -169,7 +170,8 @@ void Locomotor::doCostmapUpdate(nav_core2::Costmap& costmap, Executor& result_ex
   }
   catch (const nav_core2::CostmapException& e)
   {
-    if (fail_cb) result_ex.addCallback(std::bind(fail_cb, e, getTimeDiffFromNow(start_t)));
+    if (fail_cb)
+      result_ex.addCallback(std::bind(fail_cb, std::current_exception(), getTimeDiffFromNow(start_t)));
   }
 }
 
@@ -189,7 +191,8 @@ void Locomotor::makeGlobalPlan(Executor& result_ex, GlobalPlanCallback cb, Plann
   // if we didn't get a plan and we are in the planning state (the robot isn't moving)
   catch (const nav_core2::PlannerException& e)
   {
-    if (fail_cb) result_ex.addCallback(std::bind(fail_cb, e, getTimeDiffFromNow(start_t)));
+    if (fail_cb)
+      result_ex.addCallback(std::bind(fail_cb, std::current_exception(), getTimeDiffFromNow(start_t)));
   }
 }
 
@@ -222,7 +225,8 @@ void Locomotor::makeLocalPlan(Executor& result_ex, LocalPlanCallback cb, Planner
     catch (const nav_core2::PlannerException& e)
     {
       lock.unlock();
-      if (fail_cb) result_ex.addCallback(std::bind(fail_cb, e, getTimeDiffFromNow(start_t)));
+      if (fail_cb)
+        result_ex.addCallback(std::bind(fail_cb, std::current_exception(), getTimeDiffFromNow(start_t)));
     }
   }
 }
